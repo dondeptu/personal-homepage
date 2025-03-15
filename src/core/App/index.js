acbee8962aff93f7453b18ed53d8a641548c5cd4
Donata Deptula <dondeptu@gmail.com>
2025-03-15 11:20:27 +0100

    added Normalize to App komponent

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -2,9 +2,11 @@ import { ThemeProvider } from "styled-components";
 import { LightMode } from "./theme";
 import { GlobalStyle } from "./GlobalStyle";
 import { PersonalHomepage } from "../../features/personalHomepage";
+import { Normalize } from "styled-normalize";
 
 export const App = () => (
     <ThemeProvider theme={LightMode}>
+        <Normalize />
         <GlobalStyle />
         <PersonalHomepage />
     </ThemeProvider>
