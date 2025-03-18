5a6e62d7f28654c82c7505209d8417ce90101df9
Donata Deptula <dondeptu@gmail.com>
2025-03-18 23:06:43 +0100

    add sorting by creation date to GitHub repo fetch

--- src/features/personalHomepage/repositoriesAPI.js
+++ src/features/personalHomepage/repositoriesAPI.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const githubBaseURL = "https://api.github.com";
 
 export const getRepositories = username =>
-    axios.get(`${githubBaseURL}/users/${username}/repos`)
+    axios.get(`${githubBaseURL}/users/${username}/repos?sort=created`)
         .then(response => response.data)
         .catch(error => console.error(error));
