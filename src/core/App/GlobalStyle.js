1d1d83f94643251f7038894c5a10c97aa311afbf
Donata Deptula <dondeptu@gmail.com>
2025-03-22 21:17:37 +0100

    added padding-top for body and padding-top/bottom media queries

--- src/core/App/GlobalStyle.js
+++ src/core/App/GlobalStyle.js
@@ -19,11 +19,23 @@ export const GlobalStyle = createGlobalStyle`
         font-size: 18px;
         letter-spacing: 0.05em;
         word-break: break-word;
+        padding-top: 119px;
         padding-bottom: 109px;
         transition: background 0.3s;
+
+        @media(max-width: ${({ theme }) => theme.breakpoints.smallDesktopMax}px) {
+            padding-top: 68px;
+            padding-bottom: 78px;
+        }
+
+        @media(max-width: ${({ theme }) => theme.breakpoints.tabletMax}px) {
+            padding-top: 56px;
+            padding-bottom: 42px;
+        }
         
         @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
             font-size: 14px;
+            padding-top: 34px;
             padding-bottom: 31px;
         }
     }
